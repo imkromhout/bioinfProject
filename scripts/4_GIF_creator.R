15af319a5de08aee49c332dc80b63baf186ea0d4
Iris Kromhout Van Der Meer <imkromhout@gmail.com>
2016-01-29 16:59:26 +0100

    Update 4_GIF_creator.R
    additional comments

--- scripts/4_GIF_creator.R
+++ scripts/4_GIF_creator.R
@@ -5,19 +5,29 @@ setwd("~/scripts")  # set working directory
 ## load package
 library(animation)
 
+# create a list of 3 elements that represent the three categories
 categoryList <- c(1,2,3) # 1=budburst, 2=first_egg, 3=interval
 
+# loop over categories
 for (category in categoryList){
+    # if the category is 1 (budburst)..
     if (category == 1){
+    # ..the files that should be imported are:
       ImportFiles <- "budburst_map_*.png"
+    # ..the file that should be created and outputted is:
       OutputFile <- "budburst.gif"
-    } else if (category == 2){
+    } else if (category == 2){      # if the category is not 1: if it is 2..
+    # ..the files that should be imported are:
       ImportFiles <- "egg_map_*.png"
+    # ..the file that should be created and outputted is:
       OutputFile <- "egg.gif"
-    } else if (category == 3){
+    } else if (category == 3){      # if the category is not 1 and it is not 2: if it is 3..
+    # ..the files that should be imported are:
       ImportFiles <- "interval_map_*.png"
+    # ..the file that should be created and outputted is:
       OutputFile <- "interval.gif"
     }
+    ## convert (for every category, because still in for loop) all Importfiles to one Outputfile
     im.convert(files = ImportFiles, output = OutputFile, convert = c("convert", "gm convert"), 
                cmd.fun = if (.Platform$OS.type == "windows") shell else system, extra.opts = "", 
                clean = FALSE)
@@ -25,15 +35,3 @@ for (category in categoryList){
 
 
 
-#################### SKETCH AREA ############################
-#im.convert(files = 'budburst_map_*.png', output = "budburst.gif", convert = c("convert", "gm convert"), 
-#           cmd.fun = if (.Platform$OS.type == "windows") shell else system, extra.opts = "", 
-#           clean = FALSE)
-
-#im.convert(files = 'egg_map_*.png', output = "first_egg.gif", convert = c("convert", "gm convert"), 
-#           cmd.fun = if (.Platform$OS.type == "windows") shell else system, extra.opts = "", 
-#           clean = FALSE)
-
-#im.convert(files = 'interval_map_*.png', output = "interval.gif", convert = c("convert", "gm convert"), 
-#           cmd.fun = if (.Platform$OS.type == "windows") shell else system, extra.opts = "", 
-#           clean = FALSE)
